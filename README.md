e45867eae4a4e8054838dfd260dbc4ba9d7a439c
limalinuxos <limalinuxos@proton.me>
2026-03-06 19:47:01 +0100

    update

---
+++ README.md
@@ -0,0 +1,16 @@
+# Limalinux calamares config
+
+![Limalinux Logo](limalinux.png)
+
+**Limalinux** is a customizable Arch Linux ISO builder based on the [KIRO](https://github.com/kirodubes) project. It provides a simple way to build your own Arch-based installation medium with your choice of packages, settings, and scripts. Compare the configs and search for references like ArcoLinux, arcolinux, kiro, arconet and ArcoNet and change them to Limalinux in this case. Use tools like rg (from ripgrep) and meld to compare.
+
+## 🚀 Features
+
+- Fully customizable build process
+- Based on official Arch Linux tools and methodology
+- GUI from https://github.com/calamares/calamares
+- Script-driven: reproducible and automatable
+- Modular structure for easy extension
+- Lightweight and minimal by default
+
+
---
+++ archiso/profiledef.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="limalinux"
+iso_label="limalinux_v26.03.00.01"
+iso_publisher="limalinux"
+iso_application="limalinux Live/Rescue CD"
+iso_version="v26.03.00.01"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux'
+           'uefi.systemd-boot')
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '6')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
+file_permissions=(
+  ["/etc/gshadow"]="0:0:400"
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/root/.gnupg"]="0:0:700"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/grub.d/40_custom"]="0:0:755"
+)
